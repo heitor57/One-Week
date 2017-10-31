550e38db73cf454d68faa2866d4df15d84d684e2
Reymax <reymax619@gmail.com>
2017-10-31 18:11:52 -0200

    refeito as mudanças do menu pause

--- Assets/Menus/MenuPause.cs
+++ Assets/Menus/MenuPause.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class MenuPause : MonoBehaviour {
 	[SerializeField] GameObject Tela;
 	[SerializeField]  bool Pausado;
-
+	//private PlayerInventory inventario;
 
 	// Use this for initialization
 	void Start () {
 		Tela.SetActive(false);
+		//inventario = GameObject.Find ("Player").GetComponent<PlayerInventory>();
+		MonoBehaviour[] components;
 		Pausado=false;
 		Time.timeScale=1;
 	}
@@ -27,21 +29,27 @@ public class MenuPause : MonoBehaviour {
 
 		}
 	}
-
-	public void Pause(){
+	private void paraTudo(bool m){
+		MonoBehaviour[] components = GameObject.FindObjectsOfType<MonoBehaviour>();
+		foreach(MonoBehaviour component in components){
+			if( (MenuPause)component != null )
+				component.enabled= m;
+		}
+	}
+	public void Pause(){		
 		Tela.SetActive(true);
+		//paraTudo (false);		
 		//Cursor.visible = true; -Moysés
 		Screen.lockCursor=false;
 		Time.timeScale=0;
-
 	}
 
 	public void Retomar(){
 		Tela.SetActive(false);
+		//paraTudo (true);
 		//Cursor.visible = false;  -Moysés
 		Screen.lockCursor=true;
 		Time.timeScale=1;
-
 	}
 	public void Sair(){
 		SceneManager.LoadScene("Loader");

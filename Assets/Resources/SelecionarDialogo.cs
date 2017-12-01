c9e4b01ff920f63d83f67314c6aa19f7281966d4
Reymax <reymax619@gmail.com>
2017-12-01 13:28:02 -0200

    Mapa arrumado + Skills v0.1 + diretorio absoluto arrumado

--- Assets/Resources/SelecionarDialogo.cs
+++ Assets/Resources/SelecionarDialogo.cs
@@ -6,9 +6,8 @@ using System.IO;
 
 public class SelecionarDialogo  {
 	public static TextAsset selecionar(){
-		string[] fileArray = Directory.GetFiles("C:\\Users\\moyse\\Desktop\\Projeto Fase 1\\Assets\\Resources","*.xml");
-
-
+		
+		string[] fileArray = Directory.GetFiles(Application.dataPath+"\\Resources","*.xml");
         System.Random rnd = new System.Random();
         int i = rnd.Next(0, fileArray.Length);
         TextAsset tex = (TextAsset)Resources.Load(fileArray[i].Split('\\')[fileArray[i].Split('\\').Length - 1].Split('.')[0]);
